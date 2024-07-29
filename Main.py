3c06120919f99f2fae38da44a348759b95fa3e5e
PunithKumar2902 <pulicharlapunith@gmail.com>
2024-07-29 10:33:38 +0530

    check shapes

--- Main.py
+++ Main.py
@@ -33,7 +33,8 @@ def train_epoch(model, user_dl, optimizer, opt):
         """ prepare data """
         #This passes user trajectory, no of times visited in tuning data, tuning trajectory
 
-        print("batch shape : ",batch.size())
+        pun = batch.to(opt.device)
+        print("batch shape : ",pun.size())
 
         event_type, event_time, test_label = map(lambda x: x.to(opt.device), batch)
 
