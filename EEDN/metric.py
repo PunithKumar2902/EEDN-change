c476395344f9608623231652cd45e8822cbdddf4
PunithKumar2902 <pulicharlapunith@gmail.com>
2024-07-30 10:58:59 +0530

    again

--- EEDN/metric.py
+++ EEDN/metric.py
@@ -93,7 +93,7 @@ def pre_rec_top(pre, rec, map_, ndcg, prediction, label__, label, event_type):
     print("\nInside pre rec top\n")
     for i, e in enumerate(event_type):
         print(i,e)
-        # e = e[e!=0]-1
+        e = e[e!=0]-1
         # target_[i][e] = 0
 
     # print("prediction size : ",prediction.size())
